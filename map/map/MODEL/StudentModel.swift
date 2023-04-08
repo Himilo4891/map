122cd452891fcada66b95fc089a16f78276aea23
Himilo4891 <118472464+Himilo4891@users.noreply.github.com>
2023-04-08 12:29:57 +0300

    Delete map directory

--- map/map/MODEL/StudentModel.swift
+++
@@ -1,9 +0,0 @@
-
-import Foundation
-struct StudentModel {
-    static var locations = [StudentLocation]()
-    static var link : String = ""
-    static var location : String = ""
-    static var latitude : Double = 0.0
-    static var longitude: Double = 0.0
-}
